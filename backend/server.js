49f39af48bb74db93e805070dd20a47a2db13dbd
Rajeshwar <kolimirajeshwarreddy@gmail.com>
2023-08-10 18:38:49 +0530

    config changes

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(session({
 }));
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'https://main--cerulean-monstera-3ccddd.netlify.app',
         credentials: true,
     })
 );
